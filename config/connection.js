d1ebecb567d9b554a5eaf9347aeab30637263915
tam <taam.tr17@gmail.com>
2019-01-04 10:42:46 -0800

    added JAWSDBurl

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "tam89",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "tam89",
+        database: "burgers_db"
+    });
+};
 
 // Make connection
 connection.connect(function (err) {
@@ -15,7 +20,7 @@ connection.connect(function (err) {
         console.erro("Error connecting: " + err.stack);
         return;
     }
-    console.log("Connected as id: " + connection.threadId); 
+    console.log("Connected as id: " + connection.threadId);
 });
 
 // Export connection for our ORM to use
